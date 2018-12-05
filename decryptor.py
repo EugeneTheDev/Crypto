523d55faf917c4ec2637f1f78f9a76c2ba508b17
Eugene <eugene.seager@gmail.com>
2018-12-05 20:26:42 +0300

    Init commit

---
+++ decryptor.py
@@ -0,0 +1,76 @@
+import encryptor
+
+
+frequencies = {
+    "a": 0.0817,
+    "b": 0.0149,
+    "c": 0.0278,
+    "d": 0.0425,
+    "e": 0.127,
+    "f": 0.0229,
+    "g": 0.0202,
+    "h": 0.0609,
+    "i": 0.0697,
+    "j": 0.0015,
+    "k": 0.0077,
+    "l": 0.0403,
+    "m": 0.0241,
+    "n": 0.0675,
+    "o": 0.0751,
+    "p": 0.0193,
+    "q": 0.001,
+    "r": 0.0599,
+    "s": 0.0633,
+    "t": 0.0906,
+    "u": 0.0276,
+    "v": 0.0098,
+    "w": 0.0236,
+    "x": 0.0015,
+    "y": 0.0197,
+    "z": 0.0007
+}
+
+
+def frequency_analysis(text, key):
+    test_frequency = {}
+    text = encryptor.crypt(text, key)
+    for letter in text:
+        letter = letter.lower()
+        if letter in frequencies and letter not in test_frequency:
+            test_frequency[letter] = calculate_frequency(text, letter)
+    delta = 0
+    for key, value in test_frequency.items():
+        delta += abs(frequencies[key] - value)
+    return delta
+
+
+def calculate_frequency(text, letter):
+    total_number = letter_number = 0
+    for let in text:
+        total_number += 1
+        if let == letter:
+            letter_number += 1
+    return letter_number/total_number
+
+
+def analysis(text):
+    keys = {}
+    for i in range(1, len(encryptor.alphabet)):
+        keys[i] = frequency_analysis(text, -i)
+    sorted_keys = sorted(keys.items(), key=lambda kv: kv[1])
+    print(sorted_keys)
+    return sorted_keys[0][0]
+
+
+def decrypt():
+    text = encryptor.read_file(encryptor.cipher_url)
+    key = analysis(text)
+    open_text = encryptor.crypt(text, -key)
+    encryptor.write_file(encryptor.open_text_url, open_text)
+    print("Decrypted with key", key)
+
+
+if __name__ == '__main__':
+    decrypt()
+
+
---
+++ encryptor.py
@@ -0,0 +1,57 @@
+"""This module used both for encryption and decryption"""
+
+from string import ascii_letters, digits
+from math import fmod
+
+text_url = "data/text.txt"
+cipher_url = "data/cipher.txt"
+open_text_url = "data/open_text.txt"
+alphabet = list(ascii_letters + digits) + [".", ",", "!", "?", ";", "-", ":", "_"]
+
+
+class Dictionary:
+
+    def __init__(self, key):
+        self.key = key
+
+    def get_symbol(self, old):
+        index = alphabet.index(old) + self.key
+        if abs(index) < len(alphabet):
+            return alphabet[index]
+        elif index > 0:
+            return alphabet[index - len(alphabet)]
+
+
+def read_file(file):
+    with open(file) as f:
+        text = ""
+        for line in f.readlines():
+            text += line
+    return text
+
+
+def write_file(file, text: str):
+    with open(file, "w+") as f:
+        f.writelines(text.splitlines(keepends=True))
+
+
+def crypt(text, key):
+    encrypted_text = ""
+    dic = Dictionary(key)
+    for letter in text:
+        if letter in alphabet:
+            encrypted_text += dic.get_symbol(letter)
+        else:
+            encrypted_text += letter
+    return encrypted_text
+
+
+def encrypt(key):
+    print(key)
+    encrypted_text = crypt(read_file(text_url), key)
+    write_file(cipher_url, encrypted_text)
+
+
+if __name__ == '__main__':
+    len(alphabet)
+    encrypt(int(fmod(int(input("Input key: ")), len(alphabet))))
